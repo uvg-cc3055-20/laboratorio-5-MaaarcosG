db1eef0a390e0ae10cb8a80281b7597578a3e4b5
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2018-02-14 08:15:10 -0600

    EffectUp and EffectDown made

---
+++ Assets/Scripts/AreaEffector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+ * @author Marcos Gutierrez                 17909
+ * @since 14/02/2018
+ * Script the AreaEffector
+ * */
+public class AreaEffector : MonoBehaviour {
+    public float time = 0f;
+    public GameObject child;
+    public bool active = true;
+
+    /*Use this for initialization*/
+    void Start () {
+        //time = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*Calculo para sumar los frame*/
+        time = time + Time.deltaTime;
+
+        /*Condicion para desactivar el AreaEffector*/
+        if(time >= 3f)
+        {
+            active = !active;
+            child.gameObject.SetActive(active);
+            time = 0;
+        }
+        
+	}
+}
